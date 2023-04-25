6df5516db3c6b9b3147b4c377af5de8da734c636
Glenn Griffin <ggriffiniii@gmaill.com>
2023-04-24 23:16:39 -0700

    Not much to talk about yet

---
+++ py/sim.py
@@ -0,0 +1,28 @@
+import os
+import sorterlib
+import sys
+
+IMG_DIR = sys.argv[1]
+
+tubes = []
+
+class Tube(object):
+    def __init__(self, color):
+        self.color = color
+        self.count = 0
+
+    def __str__(self):
+        return f'Tube({self.color}, count={self.count})'
+
+for filename in sorted(os.listdir(IMG_DIR)):
+    filename = os.path.join(IMG_DIR, filename)
+    with open(filename, 'rb') as f:
+        img_data = f.read()
+    (bead_color, _) = sorterlib.bead_color(img_data)
+    (best_tube, dist) = min(((tube, sorterlib.rgb_dist(bead_color, tube.color)) for tube in tubes), key=lambda pair: pair[1], default=(None, 2**31))
+    if dist < 50 or len(tubes) == 30:
+        print(f'bead_color: {bead_color}: {dist} from {best_tube} assigning')
+        best_tube.count += 1
+    else:
+        print(f'bead_color: {bead_color}: {dist} from {best_tube} allocating new tube')
+        tubes.append(Tube(bead_color))
--- py/sorterlib.py
+++ py/sorterlib.py
@@ -14,11 +14,11 @@ def rgb_dist(a, b):
   g = a.g - b.g
   b = a.b - b.b
   return math.isqrt(
-      ((512*rmean)*r*r)>>8
+      (((512+rmean)*r*r)>>8)
       +
       4*g*g
       +
-      ((767-rmean)*b*b)>>8
+      (((767-rmean)*b*b)>>8)
   )
 
 def rgb_mean(rgb_iterable):
@@ -26,17 +26,19 @@ def rgb_mean(rgb_iterable):
   sum_rgb = RGB(0, 0, 0)
   for rgb in rgb_iterable:
     count += 1
-    sum_rgb.r += rgb.r * rgb.r
-    sum_rgb.g += rgb.g * rgb.g
-    sum_rgb.b += rgb.b * rgb.b
+    sum_rgb = RGB(
+        sum_rgb.r + (rgb.r * rgb.r),
+        sum_rgb.g + (rgb.g * rgb.g),
+        sum_rgb.b + (rgb.b * rgb.b),
+    )
 
   if count == 0:
-    return sum_rgb
+    return RGB(0,0,0)
 
   return RGB(
-    math.isqrt(sum_rgb.r / count),
-    math.isqrt(sum_rgb.g / count),
-    math.isqrt(sum_rgb.b / count),
+    math.isqrt(sum_rgb.r // count),
+    math.isqrt(sum_rgb.g // count),
+    math.isqrt(sum_rgb.b // count),
   )
 
 def bead_arc(start_pixel):
@@ -46,7 +48,7 @@ def bead_arc(start_pixel):
   assert start_y > 7
 
   idx = start_pixel
-  for _ in range(6):
+  for _ in range(5):
     idx -= IMG_WIDTH
     yield idx
 
@@ -60,29 +62,47 @@ def bead_arc(start_pixel):
   idx += IMG_WIDTH + 1
   yield idx
 
-  for _ in range(6):
+  for _ in range(5):
     idx += IMG_WIDTH
     yield idx
 
 def _rgb_for_pixel(img, pixel_idx):
   assert pixel_idx < IMG_SIZE
   idx = pixel_idx * 2
-  red = img[idx] >> 3
-  green = (img[idx] << 5) | (img[idx+1] >> 5)
-  blue = img[idx+1] & 0b11111
-  return RGB(red, green, blue)
+  r5 = img[idx] >> 3
+  g6 = ((img[idx] << 3) | (img[idx+1] >> 5)) & 0b111111
+  b5 = (img[idx+1] & 0b11111)
+
+  r8 = (r5 * 255 + 15) // 31
+  g8 = (g6 * 255 + 31) // 63
+  b8 = (b5 * 255 + 15) // 31
+  return RGB(r8, g8, b8)
 
 
 def _arc_score(img, start_pixel):
   distances = {}
 
   for (a, b) in itertools.combinations(bead_arc(start_pixel), 2):
-    dist = rgb_dist(_rgb_for_pixel(img, a), _rgb_for_pixel(img, b))
-    distances.get(a, {})[b] = dist
-    distances.get(b, {})[a] = dist
+    rgb_a = _rgb_for_pixel(img, a)
+    rgb_b = _rgb_for_pixel(img, b)
+    dist = rgb_dist(rgb_a, rgb_b)
+    a_distances = distances.get(a, {})
+    a_distances[b] = dist
+    distances[a] = a_distances
 
-  while len(distances) > 10:
-    (max_pixel, _max_dist) = max(((pixel, sum(dists)) for (pixel, dists) in distances), key=lambda pair: pair[1])
+    b_distances = distances.get(b, {})
+    b_distances[a] = dist
+    distances[b] = b_distances
+
+  while len(distances) > 12:
+    (max_pixel, _max_dist) = max(
+        (
+            (pixel, sum(dists.values()))
+            for (pixel, dists)
+            in distances.items()
+        ),
+        key=lambda pair: pair[1]
+    )
     del distances[max_pixel]
     for dists in distances.values():
       del dists[max_pixel]
@@ -91,7 +111,7 @@ def _arc_score(img, start_pixel):
 
   return (distances.keys(), total)
 
-START_PIXELS = [y*IMG_WIDTH+x for x in range(15,20) for y in range(15,20)]
+START_PIXELS = [y*IMG_WIDTH+x for x in range(15,21) for y in range(18,22)]
 
 def bead_color(img):
   (pixels, score) = min((_arc_score(img, start_pixel) for start_pixel in START_PIXELS), key=lambda pair: pair[1])
